61c833c2d3be12e59602d5c0059cc4e8b28b5081
anikkhann <anikkhann46@gmail.com>
2020-11-08 03:49:13 +0600

    volunteer-network

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -25,7 +25,7 @@ const Home = () => {
             <div >
             <h3 className="d-flex justify-content-center" style={{fontWeight:'bold'}}>I GROW BY HELPING PEOPLE IN NEED.</h3>
              <Form inline className="d-flex justify-content-center">
-                <input type="search" name="search" id="" placeholder="search"/>
+                <input style={{height:'35px'}} type="search" name="search" id="" placeholder="search"/>
                 {/* <FormControl style={{backgroundColor:'white'}} type="search" placeholder="Search"/> */}
                 <Button style={{backgroundColor:'#007bff', color:'white'}} type="submit">Search</Button>
              
